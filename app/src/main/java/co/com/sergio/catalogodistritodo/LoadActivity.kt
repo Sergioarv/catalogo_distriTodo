5881183f0997f4cbd8217f85fb1b19a6b0b023da
SERGIO RODRÍGUEZ <SARV9208@hotmail.com>
2023-06-14 20:36:42 -0500

    Se implementaron las Activity de Load y Main

---
+++ app/src/main/java/co/com/sergio/catalogodistritodo/LoadActivity.kt
@@ -0,0 +1,30 @@
+package co.com.sergio.catalogodistritodo
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Menu
+import androidx.activity.ComponentActivity
+
+/**
+ * @Author: Sergio Rodriguez Vasquez
+ * @Author: carlos Hernan Vasquez
+ * @Email: ingsergiorodriguezvasquez@gmail.com
+ * @Date: 14/06/2023
+ */
+
+class LoadActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_load);
+
+        // Tiempo de carga antes de entrar al menu
+        val TIME: Long = 3000;
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+        }, TIME);
+    }
+}
