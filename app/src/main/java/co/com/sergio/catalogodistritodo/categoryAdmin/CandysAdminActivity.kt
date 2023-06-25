cdc3786a8ba2e37f7abaca2a656fc0db42f4fc5c
SERGIO RODRÍGUEZ <SARV9208@hotmail.com>
2023-06-25 16:43:20 -0500

    Implmentacion de menus de categorias

--- app/src/main/java/co/com/sergio/catalogodistritodo/categoryAdmin/CandysAdminActivity.kt
+++ app/src/main/java/co/com/sergio/catalogodistritodo/categoryAdmin/CandysAdminActivity.kt
@@ -1,21 +1,49 @@
 package co.com.sergio.catalogodistritodo.categoryAdmin
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 import co.com.sergio.catalogodistritodo.R
-import co.com.sergio.catalogodistritodo.categoryAdmin.ui.theme.Catalogo_distriTodoTheme
 
-class CandysAdminActivity : ComponentActivity() {
+class CandysAdminActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_candys_admin)
+
+        var actionBar: ActionBar? = supportActionBar
+        actionBar?.title = "Dulces"
+        actionBar?.setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.colorPrimaryDark)));
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        var menuInflater: MenuInflater = menuInflater
+        menuInflater.inflate(R.menu.menu_agregar_item, menu)
+        menuInflater.inflate(R.menu.menu_vista_item, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.addItemBtn -> {
+                Toast.makeText(this, "Agregar item", Toast.LENGTH_SHORT).show()
+            }
+            R.id.viewItemBtn -> {
+                Toast.makeText(this, "Listar item", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/co/com/sergio/catalogodistritodo/categoryAdmin/LiquorsAdminActivity.kt
+++ app/src/main/java/co/com/sergio/catalogodistritodo/categoryAdmin/LiquorsAdminActivity.kt
@@ -2,20 +2,13 @@ package co.com.sergio.catalogodistritodo.categoryAdmin
 
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import co.com.sergio.catalogodistritodo.R
-import co.com.sergio.catalogodistritodo.categoryAdmin.ui.theme.Catalogo_distriTodoTheme
 
 class LiquorsAdminActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +19,25 @@ class LiquorsAdminActivity : AppCompatActivity() {
         actionBar?.title = "Licores"
         actionBar?.setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.colorPrimaryDark)));
         actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        var menuInflater: MenuInflater = menuInflater
+        menuInflater.inflate(R.menu.menu_agregar_item, menu)
+        menuInflater.inflate(R.menu.menu_vista_item, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.addItemBtn -> {
+                Toast.makeText(this, "Agregar item", Toast.LENGTH_SHORT).show()
+            }
+            R.id.viewItemBtn -> {
+                Toast.makeText(this, "Listar item", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onSupportNavigateUp(): Boolean {
